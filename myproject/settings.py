ed92e8a3010e64f30a68d783cf56b5853b8b071a
asmaakther2006 <asma09012006@gmail.com>
2026-08-14 18:06:28 +0530

    prepare project for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,7 +19,13 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS =[] 
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
+
 
 
 # --------------------------------------------------
